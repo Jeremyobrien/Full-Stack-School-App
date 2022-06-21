9391469b89a9e7285f41d2cb554a98da9e3adddf
@Jeremyobrien <jjob1391@gmail.com>
2022-06-21 14:07:37 -0400

    clean up comments

--- client/src/components/Context/index.js
+++ client/src/components/Context/index.js
@@ -5,7 +5,7 @@ import React, {
 }from 'react';
 import axios from 'axios';
 import {useNavigate, useParams} from 'react-router-dom';
-// import Cookies from 'js-cookie';
+
 import { Buffer } from 'buffer';
 import config from './config';
 //create context for variables and functions
@@ -29,20 +29,6 @@ export function ResultProvider({ children }) {
   const [course, setCourse] = useState();
   const { id } = useParams;
   const navigate = useNavigate();
-
-
-  /* EXAMPLE OF CREATING GLOBAL COOKIE STATE WITH JS-COOKIE V^3.01 
-  SEE 'signin' FUNCTION FURTHER DOWN FOR INITIALIZATION */
-  // const [authUser, setAuthUser] = useState([])
-  // const cookieE = Cookies.get('authEmail')
-  // const cookieP = Cookies.get('authPassword')
-  // const cookieFN = Cookies.get('authFirstName')
-  // const cookieLN = Cookies.get('authLastName')
-  // const cookieId = Cookies.get('authUserId')
-  // //Create Cookie state
-  // useEffect( () => {
-  //   setAuthUser([cookieE, cookieFN, cookieId, cookieLN, cookieP])
-  // },[cookieE, cookieFN, cookieId, cookieLN, cookieP]);
 
 //brings in list of courses on render
   useEffect( ()=> {   
@@ -104,12 +90,6 @@ export function ResultProvider({ children }) {
     if ( user !== null ) {
         user.password = password
         setUser(user)
-        // /*EXAMPLE OF INITIALIZING COOKIES WITH JS-COOKIE V^3.01*/
-        // Cookies.set('authEmail', JSON.stringify(user.emailAddress), { expires: 1});
-        // Cookies.set('authPassword', JSON.stringify(user.password), {expires: 1});
-        // Cookies.set('authFirstName', JSON.stringify(user.firstName), {expires: 1});
-        // Cookies.set('authLastName', JSON.stringify(user.lastName), {expires: 1});
-        // Cookies.set('authUserId', JSON.parse(user.id), {expires: 1});
     } else {
         return;
     }
--- client/src/components/Forbidden.js
+++ client/src/components/Forbidden.js
@@ -8,7 +8,7 @@ const Forbidden = () => {
         <main>
             <div className="wrap">
                 <h2>Forbidden</h2>
-                <p>Oh oh! You can't access this page.</p>
+                <p>Uh oh! You can't access this page.</p>
             </div>
         </main>
         </>
